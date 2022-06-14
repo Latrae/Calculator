5aadd8b14923f3f85046b28381d2dfc4d679b586
LaTrae' Swann <104823604+Latrae@users.noreply.github.com>
2022-06-13 23:45:42 -0400

    Add files via upload

--- Calculator/CalApp.cpp
+++ Calculator/CalApp.cpp
@@ -1,1 +1,21 @@
 #include "CalApp.h"
+
+wxIMPLEMENT_APP(CalApp);
+
+
+CalApp::CalApp()
+{
+
+}
+
+CalApp::~CalApp()
+{
+
+}
+
+bool CalApp::OnInit()
+{
+	m_frame1 = new CalUI();
+	m_frame1->Show();
+	return true;
+}
--- Calculator/CalApp.h
+++ Calculator/CalApp.h
@@ -1,5 +1,20 @@
 #pragma once
-class CalApp
-{
-};
 
+
+#include "wx/wx.h"
+#include "CalUI.h"
+
+
+
+
+class CalApp : public wxApp
+{
+	virtual bool OnInit();
+
+private:
+	CalUI* m_frame1 = nullptr;
+
+public:
+	CalApp();
+	~CalApp();
+};
